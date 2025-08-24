0eba20ac12199f35a2ff056fd5a73a48ecdabddd
Ion <ionpetro@gmail.com>
2025-08-24 14:59:17 -0500

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['defdpaewkfkpvdzyhvsr.supabase.co'],
+    domains: [
+      'defdpaewkfkpvdzyhvsr.supabase.co',
+      'gamerplug-gameplay-videos.s3.us-east-2.amazonaws.com'
+    ],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'defdpaewkfkpvdzyhvsr.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'gamerplug-gameplay-videos.s3.us-east-2.amazonaws.com',
+        port: '',
+        pathname: '/users/**',
       }
     ]
   }
