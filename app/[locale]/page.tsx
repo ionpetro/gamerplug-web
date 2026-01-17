acc38f86c4ff3207d70700831b46b1e47762e351
Ion Petropoulos <ionpetro@gmail.com>
2026-01-17 15:10:50 +0200

    Add ElevenLabs ConvAI widget to homepage

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "@/components/Footer"
 import { LandingSections } from "@/components/landing/LandingSections"
+import Script from "next/script"
 
 // --- Main Page ---
 
@@ -72,6 +73,11 @@ export default function LocalizedHome() {
         <LandingSections />
       </main>
       <Footer />
+      <elevenlabs-convai agent-id="agent_1501kd5ergn1e5zr4345bsc9zs9k"></elevenlabs-convai>
+      <Script
+        src="https://unpkg.com/@elevenlabs/convai-widget-embed"
+        strategy="lazyOnload"
+      />
     </div>
   )
 }
